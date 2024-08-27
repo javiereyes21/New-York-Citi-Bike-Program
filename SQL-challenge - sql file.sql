2b1693c509b4e1d4f566e969bcd1c13cfc0a3c39
[Javier Reyes] <[javiereyes21@gmail.com]>
2024-08-27 16:02:38 -0700

    update my repo w SQL files

--- SQL-challenge - sql file.sql
+++ SQL-challenge - sql file.sql
@@ -25,10 +25,8 @@ CREATE TABLE departments (
 
 CREATE TABLE dept_manager(
 	dept_no VARCHAR NOT NULL,
-	emp_no INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (dept_no, emp_no)
+	dept_name VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE dept_emp (
